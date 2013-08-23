76fc36bdefd5c0bf22e3bac3ef316823beaf6f48
三世 忠洋 <m_tadahiro@outlook.com>
2013-08-23 21:57:00 +0900

    TimerViewに秒単位で渡すように修正。

--- SummerCamp/TopViewController.m
+++ SummerCamp/TopViewController.m
@@ -16,33 +16,36 @@
 @implementation TopViewController
 {
     BOOL isMunepoke;
-    int  timer;
+    float timer;
+    
+    NSUserDefaults *ud;
 }
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 
-    NSUserDefaults *ud = [NSUserDefaults standardUserDefaults];
+    ud = [NSUserDefaults standardUserDefaults];
     
     isMunepoke = [ud boolForKey:@"mode"];
     timer = [ud integerForKey:@"timer"];
     
     [self setButtonEnabled];
     
-    self.timerLabel.text = [NSString stringWithFormat:@"%d",timer];
+    self.timerLabel.text = [NSString stringWithFormat:@"%f",timer];
 }
 
 - (void)setButtonEnabled {
+    
+    // モードを登録
+    [ud setBool:isMunepoke forKey:@"mode"];
+    [ud synchronize];
+    
     self.munepokeButton.enabled = !isMunepoke;
     self.deskButton.enabled = isMunepoke;
     
-    
     self.munepokeButton.alpha = isMunepoke ? 0.0f : 1.0f;
     self.deskButton.alpha = !isMunepoke ? 0.0f : 1.0f;
-    
-//    self.munepokeButton.titleLabel.textColor = isMunepoke ? [UIColor whiteColor] : [UIColor blackColor];
-//    self.deskButton.titleLabel.textColor = isMunepoke ? [UIColor blackColor] : [UIColor whiteColor];
 }
 
 - (void)didReceiveMemoryWarning
@@ -52,39 +55,52 @@
 
 - (IBAction)munepoke:(id)sender {
     isMunepoke = YES;
-    
     [self setButtonEnabled];
 }
 
 - (IBAction)desk:(id)sender {
     isMunepoke = NO;
-    
     [self setButtonEnabled];
 }
 
 - (IBAction)addTime:(id)sender {
     
+    // とりあえず、Maxは30分にする
     if(timer == 30) {
         return;
     }
-    
-    self.timerLabel.text = [NSString stringWithFormat:@"%d",++timer];
+   
+    timer++;
+    [self setTimer];
 }
 
 - (IBAction)minusTime:(id)sender {
     
+    // Minは0分にする
     if(timer == 0) {
         return;
     }
+    
+    timer--;
+    [self setTimer];
+}
 
-    self.timerLabel.text = [NSString stringWithFormat:@"%d",--timer];
+- (void)setTimer {
+    
+    self.timerLabel.text = [NSString stringWithFormat:@"%f",timer];
+    
+    // モードを登録
+    [ud setInteger:timer forKey:@"timer"];
+    [ud synchronize];
 }
 
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     
     TimerViewController * controller = [segue destinationViewController];
     controller.mode = isMunepoke;
-    controller.time = timer;
+    
+    // 分を秒に変換
+    controller.time = timer * 60;
 }
 
 @end
