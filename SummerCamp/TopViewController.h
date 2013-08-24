f096815ff287f3ecfa9279d32047e0a929b50b11
kozawa <kozawa.toshiyuki@pasonatech.jp>
2013-08-24 09:47:43 +0900

    no message

--- SummerCamp/TopViewController.h
+++ SummerCamp/TopViewController.h
@@ -21,4 +21,33 @@
 - (IBAction)addTime:(id)sender;
 - (IBAction)minusTime:(id)sender;
 
+
+//デフォルトの背景
+@property (weak, nonatomic) IBOutlet UIImageView *backDef01;
+@property (weak, nonatomic) IBOutlet UIImageView *backDef02;
+@property (weak, nonatomic) IBOutlet UIImageView *backDef03;
+@property (weak, nonatomic) IBOutlet UIImageView *backDef04;
+@property (weak, nonatomic) IBOutlet UIImageView *backDef05;
+@property (weak, nonatomic) IBOutlet UIImageView *backDef06;
+@property (weak, nonatomic) IBOutlet UIImageView *backDef07;
+
+//ポケットの背景
+@property (weak, nonatomic) IBOutlet UIImageView *backPoke01;
+@property (weak, nonatomic) IBOutlet UIImageView *backPoke02;
+@property (weak, nonatomic) IBOutlet UIImageView *backPoke03;
+@property (weak, nonatomic) IBOutlet UIImageView *backPoke04;
+@property (weak, nonatomic) IBOutlet UIImageView *backPoke05;
+@property (weak, nonatomic) IBOutlet UIImageView *backPoke06;
+@property (weak, nonatomic) IBOutlet UIImageView *backPoke07;
+
+//机の背景
+@property (weak, nonatomic) IBOutlet UIImageView *backDesk01;
+@property (weak, nonatomic) IBOutlet UIImageView *backDesk02;
+@property (weak, nonatomic) IBOutlet UIImageView *backDesk03;
+@property (weak, nonatomic) IBOutlet UIImageView *backDesk04;
+@property (weak, nonatomic) IBOutlet UIImageView *backDesk05;
+@property (weak, nonatomic) IBOutlet UIImageView *backDesk06;
+@property (weak, nonatomic) IBOutlet UIImageView *backDesk07;
+
+
 @end
--- SummerCamp/TopViewController.m
+++ SummerCamp/TopViewController.m
@@ -19,6 +19,8 @@
     float timer;
     
     NSUserDefaults *ud;
+    
+    int backgroundFlag;
 }
 
 - (void)viewDidLoad
@@ -33,6 +35,9 @@
     [self setButtonEnabled];
     
     self.timerLabel.text = [NSString stringWithFormat:@"%d分",(int)timer];
+    
+    backgroundFlag = 0;
+     [self backGroundSet]; //背景セットへ
     
 }
 
@@ -52,16 +57,6 @@
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-}
-
-- (IBAction)munepoke:(id)sender {
-    isMunepoke = YES;
-    [self setButtonEnabled];
-}
-
-- (IBAction)desk:(id)sender {
-    isMunepoke = NO;
-    [self setButtonEnabled];
 }
 
 - (IBAction)addTime:(id)sender {
@@ -107,5 +102,119 @@
 - (IBAction)back:(UIStoryboardSegue *)sender
 {
 }
+
+//胸ポケ押した。
+- (IBAction)munepoke:(id)sender {
+    isMunepoke = YES;
+    [self setButtonEnabled];
+    backgroundFlag = 1;
+    [self backGroundSet]; //背景セットへ
+}
+
+//机モード押した。
+- (IBAction)desk:(id)sender {
+    isMunepoke = NO;
+    [self setButtonEnabled];
+    backgroundFlag = 2;
+    [self backGroundSet]; //背景セットへ
+}
+
+-(void)backGroundSet{
+    
+    switch (backgroundFlag) {
+        case 0:
+            self.backDef01.frame = CGRectMake(self.backDef01.frame.origin.x, self.backDef01.frame.origin.y, self.backDef01.frame.size.width, self.backDef01.frame.size.height);
+            self.backDef02.frame = CGRectMake(self.backDef02.frame.origin.x, self.backDef02.frame.origin.y, self.backDef02.frame.size.width, self.backDef02.frame.size.height);
+            self.backDef03.frame = CGRectMake(self.backDef03.frame.origin.x, self.backDef03.frame.origin.y, self.backDef03.frame.size.width, self.backDef03.frame.size.height);
+            self.backDef04.frame = CGRectMake(self.backDef04.frame.origin.x, self.backDef04.frame.origin.y, self.backDef04.frame.size.width, self.backDef04.frame.size.height);
+            self.backDef05.frame = CGRectMake(self.backDef05.frame.origin.x, self.backDef05.frame.origin.y, self.backDef05.frame.size.width, self.backDef05.frame.size.height);
+            self.backDef06.frame = CGRectMake(self.backDef06.frame.origin.x, self.backDef06.frame.origin.y, self.backDef06.frame.size.width, self.backDef06.frame.size.height);
+            self.backDef07.frame = CGRectMake(self.backDef07.frame.origin.x, self.backDef07.frame.origin.y, self.backDef07.frame.size.width, self.backDef07.frame.size.height);
+            break;
+            
+        case 1:
+            self.backPoke01.frame = CGRectMake(-320, self.backPoke01.frame.origin.y, self.backPoke01.frame.size.width, self.backPoke01.frame.size.height);
+            self.backPoke02.frame = CGRectMake(320, self.backPoke02.frame.origin.y, self.backPoke02.frame.size.width, self.backPoke02.frame.size.height);
+            self.backPoke03.frame = CGRectMake(-320, self.backPoke03.frame.origin.y, self.backPoke03.frame.size.width, self.backPoke03.frame.size.height);
+            self.backPoke04.frame = CGRectMake(320, self.backPoke04.frame.origin.y, self.backPoke04.frame.size.width, self.backPoke04.frame.size.height);
+            self.backPoke05.frame = CGRectMake(-320, self.backPoke05.frame.origin.y, self.backPoke05.frame.size.width, self.backPoke05.frame.size.height);
+            self.backPoke06.frame = CGRectMake(320, self.backPoke06.frame.origin.y, self.backPoke06.frame.size.width, self.backPoke06.frame.size.height);
+            self.backPoke07.frame = CGRectMake(-320, self.backPoke07.frame.origin.y, self.backPoke07.frame.size.width, self.backPoke07.frame.size.height);
+            
+            self.backDef01.frame = CGRectMake(320, self.backDef01.frame.origin.y, self.backDef01.frame.size.width, self.backDef01.frame.size.height);
+            self.backDef02.frame = CGRectMake(-320, self.backDef02.frame.origin.y, self.backDef02.frame.size.width, self.backDef02.frame.size.height);
+            self.backDef03.frame = CGRectMake(320, self.backDef03.frame.origin.y, self.backDef03.frame.size.width, self.backDef03.frame.size.height);
+            self.backDef04.frame = CGRectMake(-320, self.backDef04.frame.origin.y, self.backDef04.frame.size.width, self.backDef04.frame.size.height);
+            self.backDef05.frame = CGRectMake(320, self.backDef05.frame.origin.y, self.backDef05.frame.size.width, self.backDef05.frame.size.height);
+            self.backDef06.frame = CGRectMake(-320, self.backDef06.frame.origin.y, self.backDef06.frame.size.width, self.backDef06.frame.size.height);
+            self.backDef07.frame = CGRectMake(320, self.backDef07.frame.origin.y, self.backDef07.frame.size.width, self.backDef07.frame.size.height);
+            
+            self.backDesk01.frame = CGRectMake(-320, self.backPoke01.frame.origin.y, self.backPoke01.frame.size.width, self.backPoke01.frame.size.height);
+            self.backDesk02.frame = CGRectMake(320, self.backPoke02.frame.origin.y, self.backPoke02.frame.size.width, self.backPoke02.frame.size.height);
+            self.backDesk03.frame = CGRectMake(-320, self.backPoke03.frame.origin.y, self.backPoke03.frame.size.width, self.backPoke03.frame.size.height);
+            self.backDesk04.frame = CGRectMake(320, self.backPoke04.frame.origin.y, self.backPoke04.frame.size.width, self.backPoke04.frame.size.height);
+            self.backDesk05.frame = CGRectMake(-320, self.backPoke05.frame.origin.y, self.backPoke05.frame.size.width, self.backPoke05.frame.size.height);
+            self.backDesk06.frame = CGRectMake(320, self.backPoke06.frame.origin.y, self.backPoke06.frame.size.width, self.backPoke06.frame.size.height);
+            self.backDesk07.frame = CGRectMake(-320, self.backPoke07.frame.origin.y, self.backPoke07.frame.size.width, self.backPoke07.frame.size.height);
+            //ウィンドウ右にアニメーションして移動
+            [UIView beginAnimations:nil context:nil];
+            [UIView setAnimationDuration:0.5];
+            [UIView setAnimationCurve:UIViewAnimationCurveEaseIn];
+            self.backPoke01.frame = CGRectMake(0, self.backPoke01.frame.origin.y, self.backPoke01.frame.size.width, self.backPoke01.frame.size.height);
+            self.backPoke02.frame = CGRectMake(0, self.backPoke02.frame.origin.y, self.backPoke02.frame.size.width, self.backPoke02.frame.size.height);
+            self.backPoke03.frame = CGRectMake(0, self.backPoke03.frame.origin.y, self.backPoke03.frame.size.width, self.backPoke03.frame.size.height);
+            self.backPoke04.frame = CGRectMake(0, self.backPoke04.frame.origin.y, self.backPoke04.frame.size.width, self.backPoke04.frame.size.height);
+            self.backPoke05.frame = CGRectMake(0, self.backPoke05.frame.origin.y, self.backPoke05.frame.size.width, self.backPoke05.frame.size.height);
+            self.backPoke06.frame = CGRectMake(0, self.backPoke06.frame.origin.y, self.backPoke06.frame.size.width, self.backPoke06.frame.size.height);
+            self.backPoke07.frame = CGRectMake(0, self.backPoke07.frame.origin.y, self.backPoke07.frame.size.width, self.backPoke07.frame.size.height);
+            
+            [UIView commitAnimations];
+            break;
+            
+        case 2:
+            
+            self.backDesk01.frame = CGRectMake(-320, self.backPoke01.frame.origin.y, self.backPoke01.frame.size.width, self.backPoke01.frame.size.height);
+            self.backDesk02.frame = CGRectMake(320, self.backPoke02.frame.origin.y, self.backPoke02.frame.size.width, self.backPoke02.frame.size.height);
+            self.backDesk03.frame = CGRectMake(-320, self.backPoke03.frame.origin.y, self.backPoke03.frame.size.width, self.backPoke03.frame.size.height);
+            self.backDesk04.frame = CGRectMake(320, self.backPoke04.frame.origin.y, self.backPoke04.frame.size.width, self.backPoke04.frame.size.height);
+            self.backDesk05.frame = CGRectMake(-320, self.backPoke05.frame.origin.y, self.backPoke05.frame.size.width, self.backPoke05.frame.size.height);
+            self.backDesk06.frame = CGRectMake(320, self.backPoke06.frame.origin.y, self.backPoke06.frame.size.width, self.backPoke06.frame.size.height);
+            self.backDesk07.frame = CGRectMake(-320, self.backPoke07.frame.origin.y, self.backPoke07.frame.size.width, self.backPoke07.frame.size.height);
+            
+            self.backDef01.frame = CGRectMake(320, self.backDef01.frame.origin.y, self.backDef01.frame.size.width, self.backDef01.frame.size.height);
+            self.backDef02.frame = CGRectMake(-320, self.backDef02.frame.origin.y, self.backDef02.frame.size.width, self.backDef02.frame.size.height);
+            self.backDef03.frame = CGRectMake(320, self.backDef03.frame.origin.y, self.backDef03.frame.size.width, self.backDef03.frame.size.height);
+            self.backDef04.frame = CGRectMake(-320, self.backDef04.frame.origin.y, self.backDef04.frame.size.width, self.backDef04.frame.size.height);
+            self.backDef05.frame = CGRectMake(320, self.backDef05.frame.origin.y, self.backDef05.frame.size.width, self.backDef05.frame.size.height);
+            self.backDef06.frame = CGRectMake(-320, self.backDef06.frame.origin.y, self.backDef06.frame.size.width, self.backDef06.frame.size.height);
+            self.backDef07.frame = CGRectMake(320, self.backDef07.frame.origin.y, self.backDef07.frame.size.width, self.backDef07.frame.size.height);
+            
+            self.backPoke01.frame = CGRectMake(-320, self.backPoke01.frame.origin.y, self.backPoke01.frame.size.width, self.backPoke01.frame.size.height);
+            self.backPoke02.frame = CGRectMake(320, self.backPoke02.frame.origin.y, self.backPoke02.frame.size.width, self.backPoke02.frame.size.height);
+            self.backPoke03.frame = CGRectMake(-320, self.backPoke03.frame.origin.y, self.backPoke03.frame.size.width, self.backPoke03.frame.size.height);
+            self.backPoke04.frame = CGRectMake(320, self.backPoke04.frame.origin.y, self.backPoke04.frame.size.width, self.backPoke04.frame.size.height);
+            self.backPoke05.frame = CGRectMake(-320, self.backPoke05.frame.origin.y, self.backPoke05.frame.size.width, self.backPoke05.frame.size.height);
+            self.backPoke06.frame = CGRectMake(320, self.backPoke06.frame.origin.y, self.backPoke06.frame.size.width, self.backPoke06.frame.size.height);
+            self.backPoke07.frame = CGRectMake(-320, self.backPoke07.frame.origin.y, self.backPoke07.frame.size.width, self.backPoke07.frame.size.height);
+            
+            //ウィンドウ右にアニメーションして移動
+            [UIView beginAnimations:nil context:nil];
+            [UIView setAnimationDuration:0.5];
+            [UIView setAnimationCurve:UIViewAnimationCurveEaseIn];
+            
+            self.backDesk01.frame = CGRectMake(0, self.backPoke01.frame.origin.y, self.backPoke01.frame.size.width, self.backPoke01.frame.size.height);
+            self.backDesk02.frame = CGRectMake(0, self.backPoke02.frame.origin.y, self.backPoke02.frame.size.width, self.backPoke02.frame.size.height);
+            self.backDesk03.frame = CGRectMake(0, self.backPoke03.frame.origin.y, self.backPoke03.frame.size.width, self.backPoke03.frame.size.height);
+            self.backDesk04.frame = CGRectMake(0, self.backPoke04.frame.origin.y, self.backPoke04.frame.size.width, self.backPoke04.frame.size.height);
+            self.backDesk05.frame = CGRectMake(0, self.backPoke05.frame.origin.y, self.backPoke05.frame.size.width, self.backPoke05.frame.size.height);
+            self.backDesk06.frame = CGRectMake(0, self.backPoke06.frame.origin.y, self.backPoke06.frame.size.width, self.backPoke06.frame.size.height);
+            self.backDesk07.frame = CGRectMake(0, self.backPoke07.frame.origin.y, self.backPoke07.frame.size.width, self.backPoke07.frame.size.height);
+            [UIView commitAnimations];
+            break;
+    }
+    
+}
+
+
 
 @end
